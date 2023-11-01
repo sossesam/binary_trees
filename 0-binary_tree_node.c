e8face89f568653ee34cb587a256d5ffb792cf07
sossesam <sossesam@gmail.com>
2023-10-31 17:06:25 -0700

    all

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,16 +12,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 binary_tree_t *new_node;
 new_node = malloc(sizeof(binary_tree_t));
 
-if (new_node)
-{
+if (new_node == NULL)
+return NULL;
+
+
 new_node->n = value;
 new_node->parent = parent;
 new_node->left = NULL;
 new_node->right = NULL;
-}
-else
-{
-return NULL;
-};
+
+
 return (new_node);
 }
