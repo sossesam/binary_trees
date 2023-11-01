8a3f0effab3412a96cb246cfe47106bf48a09299
sossesam <sossesam@gmail.com>
2023-10-31 17:27:45 -0700

    all:

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,29 +8,23 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-	if (parent == NULL){
-		return NULL;
-	}
-	new_node = malloc(sizeof(binary_tree_t));
+if (parent == NULL)
+return (NULL);
 
-	if (new_node == NULL){
-		return NULL;
-	}
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->right = NULL;
-	new_node->left = NULL;
+new_node = binary_tree_node(parent, value);
 
-	if (parent->left == NULL){
-		parent->left = new_node;
-	}else{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
-		parent->left = new_node;
-	}
+if (parent->left == NULL)
+{
+parent->left = new_node;
+}
+else
+{
+new_node->left = parent->left;
+parent->left->parent = new_node;
+parent->left = new_node;
+}
 
-	return new_node;
-
+return (new_node);
 }
