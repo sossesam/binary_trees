fb903a1232aea4169983ded3f473486abf1e513c
sossesam <sossesam@gmail.com>
2023-11-02 16:29:33 -0700

    all

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_preorder - Write a function that goes through a binary
+ * tree using pre-order traversal
+ * @tree: The root node to be deleted
+ * @func - funcrtion to call
+ * Return: returns an integer
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL)
+return;
+if (func == NULL)
+return;
+
+(func)(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
+}
+
