083a28cea30d7e32a8faf398b52362dfa1e8d119
sossesam <sossesam@gmail.com>
2023-11-04 03:19:48 -0700

    all

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,24 +8,21 @@
  * Return: Always 0 (Success)
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t count;
-	count = 0;
-
-	if (tree == NULL)
-		return 0;
+size_t binary_tree_height(const binary_tree_t *tree){
+	size_t ldepth;
+	size_t rdepth;
 	
-	if(tree->left){
-	count++;
-	binary_tree_height(tree->left);
-	}
+	if (tree == NULL)
+	return 0;
 
-	if (tree->right){
-	count++;
-	binary_tree_height(tree->right);
-	}
+	if (tree->right == NULL && tree->left == NULL)
+	return 0;
 
+	ldepth = binary_tree_height(tree->left) + 1;
+	rdepth = binary_tree_height(tree->right) + 1;
 
-return count;
+	if (ldepth > rdepth){
+	return ldepth ;}
+	else{
+		return rdepth;}
 }
